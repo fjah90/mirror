c3e0b2e5b4886e857242f45f97239b71933fe3c7
Omar Herrera <omar.herrera@tigears.com>
2019-03-13 09:55:14 -0600

    agregado paquete spatie/permissions

--- config/app.php
+++ config/app.php
@@ -177,6 +177,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+
+        /*
+         * Third Parties
+         */
+        Spatie\Permission\PermissionServiceProvider::class,
     ],
 
     /*
