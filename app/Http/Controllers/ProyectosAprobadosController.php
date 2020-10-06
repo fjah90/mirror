4ac26253e4c071e1ba34d1796b6ce2077837751b
Eduardo <Eduardo.santana@tigears.com>
2020-10-06 17:55:39 -0500

    ajuste fechas

--- app/Http/Controllers/ProyectosAprobadosController.php
+++ app/Http/Controllers/ProyectosAprobadosController.php
@@ -32,7 +32,7 @@ class ProyectosAprobadosController extends Controller
     public function listado(Request $request)
     {
       if ($request->id == 'Todos') {
-        $proyectos = ProyectoAprobado::with('cotizacion','cliente','cotizacion.cuenta_cobrar')->get();
+        $proyectos = ProyectoAprobado::with('cotizacion.*','cliente')->get();
       } else {
         $user = User::with('proyectos_aprobados.cotizacion', 'proyectos_aprobados.cliente','proyectos_aprobados.cotizacion.cuenta_cobrar')->find($request->id);
         if (is_null($user)) $proyectos = [];
