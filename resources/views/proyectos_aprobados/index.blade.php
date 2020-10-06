bb90a23f30a2612aadca91164628baaefee92ea7
Eduardo <Eduardo.santana@tigears.com>
2020-10-06 18:04:14 -0500

    ajuste fechas

--- resources/views/proyectos_aprobados/index.blade.php
+++ resources/views/proyectos_aprobados/index.blade.php
@@ -59,8 +59,10 @@
                   <td>@{{proyecto.proyecto}}</td>
                   <td>@{{proyecto.cotizacion_id}}</td>
                   {{--<td>@{{proyecto.created_at|date}}</td>--}}
-                  <td v-if="proyecto.cotizacion.cuenta_cobrar !== undefined && proyecto.cotizacion.cuenta_cobrar.fecha_comprobante !== undefined && proyecto.cotizacion.cuenta_cobrar.fecha_comprobante !== null">@{{proyecto.cotizacion.cuenta_cobrar.fecha_comprobante|date}}</td>
-                  <td v-if="proyecto.cotizacion.cuenta_cobrar === undefined ||  proyecto.cotizacion.cuenta_cobrar.fecha_comprobante === undefined || proyecto.cotizacion.cuenta_cobrar.fecha_comprobante === null">@{{proyecto.created_at|date}}</td>
+                  <td v-if="proyecto.cotizacion.cuenta_cobrar !== null && proyecto.cotizacion.cuenta_cobrar !== undefined && proyecto.cotizacion.cuenta_cobrar.fecha_comprobante !== undefined && proyecto.cotizacion.cuenta_cobrar.fecha_comprobante !== null">@{{proyecto.cotizacion.cuenta_cobrar.fecha_comprobante|date}}</td>
+                  <td v-if="
+                  proyecto.cotizacion.cuenta_cobrar === null
+                  proyecto.cotizacion.cuenta_cobrar === undefined ||  proyecto.cotizacion.cuenta_cobrar.fecha_comprobante === undefined || proyecto.cotizacion.cuenta_cobrar.fecha_comprobante === null">@{{proyecto.created_at|date}}</td>
 
                   <td>
                     <span v-for="(proveedor, index) in proyecto.proveedores">
