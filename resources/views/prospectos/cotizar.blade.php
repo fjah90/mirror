20eea3dd432ba0b3b9fd81c570b59739058fdf95
Eduardo <Eduardo.santana@tigears.com>
2021-03-03 19:29:17 -0600

    ajuste fotos

--- resources/views/prospectos/cotizar.blade.php
+++ resources/views/prospectos/cotizar.blade.php
@@ -1800,7 +1800,7 @@
                     totalcotizacion = cotizacion.subtotal.toFixed(2);
                     totalf = totalf.toFixed(2)
 
-                    console.log(cotizacion.subtotal);
+                    console.log(totalcotizacion);
                     console.log(totalf);
                     console.log()
 
