ec4909ababddda697017361837235a59a1b78f7c
Eduardo <Eduardo.santana@tigears.com>
2021-03-17 19:01:14 -0600

    ajuste

--- app/User.php
+++ app/User.php
@@ -52,7 +52,7 @@ class User extends Authenticatable
 
     public function proyectos_aprobados()
     {
-        return $this->hasManyThrough('App\Models\ProyectoAprobado', 'App\Models\Cliente', 'usuario_id', 'cliente_id');
+        return $this->hasManyThrough('App\Models\ProyectoAprobado', 'App\Models\ProspectoCotizacion', 'user_id', 'cotizacion_id');
     }
 
     /*public function prospectos()
