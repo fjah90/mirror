26b8af06e6973c6ae7738dbce2f583cf168ea392
Eduardo <Eduardo.santana@tigears.com>
2021-10-18 19:11:44 -0500

    ajuste suma y bdd

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,21 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Config;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function __construct()
+    {
+        $this->middleware(function($request,$next){
+            DB::purge('mysql');
+            Config::set('database.default',session('database_name','default_database_connecction'));
+            return $next($request);
+        });
+    }
+
 }
