3e5de01f4ea68336266ad336e35e855850fa5df6
Eduardo <Eduardo.santana@tigears.com>
2021-11-01 16:07:33 -0600

    ajustes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -551,7 +551,6 @@ Dashboard | @parent
                       <th class="text-center"><strong>Proveedor</strong></th>
                       <th class="text-center"><strong>Producto</strong></th>
                       <th class="text-center"><strong>Cantidad</strong></th>
-                      <th class="text-center"><strong>Moneda</strong></th>
                       <th class="text-center"><strong>Total</strong></th>
                       <th class="text-center"><strong>Status</strong></th>
                       <th></th>
@@ -576,9 +575,6 @@ Dashboard | @parent
                             <span v-else>@{{entrada.cantidad}} @{{entrada.medida}}</span>
                           <br />
                         </span>
-                      </td>
-                      <td>
-                        @{{compra.moneda}}
                       </td>
                       <td>
                         @{{compra.total | formatoMoneda}}
@@ -710,16 +706,6 @@ Dashboard | @parent
                     </tr>
                     
                   </tbody>
-                  <tfoot>
-                  <tr>
-                      <th colspan="11" style="text-align:right">Total MXN:</th>
-                      <th></th>
-                  </tr>
-                  <tr>
-                      <th colspan="11" style="text-align:right">Total USD:</th>
-                      <th></th>
-                  </tr>
-              </tfoot>
                 </table>
               </div>
             </div>
@@ -989,36 +975,6 @@ Dashboard | @parent
         var proyectos=[]
         if (table == '#tablaCompras') {
           newTable=$(table).DataTable({
-            "footerCallback": function ( row, data, start, end, display ) {
-              var api = this.api(), data;
-
-              var formato = function ( i ) {
-                  return typeof i === 'string' ?
-                      i.replace(/[\$,]/g, '')*1 :
-                      typeof i === 'number' ?
-                          i : 0;
-              };
-              //datos de la tabla con filtros aplicados
-              var datos= api.columns([8,9], {search: 'applied'}).data();
-              var totalMxn = 0;
-              var totalUsd = 0;
-              //suma de montos
-              datos[0].forEach(function(element, index){
-                  if(datos[1][index]=="Dolares"){
-                      totalUsd+=formato(element)
-                  }else{
-                      totalMxn+=formato(element)
-                  }
-              });
-   
-              // Actualizar
-              var nCells = row.getElementsByTagName('th');
-              nCells[1].innerHTML = accounting.formatMoney(totalMxn, "$", 2);
-
-              var secondRow = $(row).next()[0]; 
-              var nCells = secondRow.getElementsByTagName('th');
-              nCells[1].innerHTML = accounting.formatMoney(totalUsd, "$", 2);
-          },
           "dom": 'f<"#'+prefix+'_fechas_container.pull-left">ltip',
             "order":[[0,'desc']],
             initComplete: function () {
@@ -1036,8 +992,7 @@ Dashboard | @parent
               this.api().column(1).data().sort().unique().each(function(d,j){   
                 proyectos.push(d);
               });
-            },
-            
+            }
           });
         }
         else{
