0f8caa587cb218f9090b69bad31c188e0afa4b54
Eduardo <Eduardo.santana@tigears.com>
2021-11-01 16:42:08 -0600

    ajustes

--- resources/views/reportes/cobros.blade.php
+++ resources/views/reportes/cobros.blade.php
@@ -67,22 +67,22 @@ Reportes | @parent
                       </template>
                     </dropdown>
                     <div class="marg025 btn-group" id="select_cotizaciones" >
-                        <select  name="proxDias" class="form-control" size="1" v-model="valor_cotizaciones" id="select_cotizaciones">
+                        <select name="proxDias" class="form-control" size="1" v-model="valor_cotizaciones" id="select_cotizaciones" :searchable="true">
                         <option v-for="(option, index) in datos_select.cotizaciones" v-bind:value="option" >
                             @{{ option }}
                           </option>
                           
                         </select>
                     </div>
-                    <div class="marg025 btn-group" id="select_proyectos2" >
-                        <select name="proxDias" class="form-control" size="1" v-model="valor_proyectos" id="select_proyectos">
+                    <div class="marg025 btn-group" id="select_proyectos" >
+                        <select name="proxDias" class="form-control" size="1" v-model="valor_proyectos" id="select_proyectos" :searchable="true">
                           <option v-for="option in datos_select.proyectos" v-bind:value="option">
                             @{{ option }}
                           </option>
                           
                         </select>
                     </div>
-                    <div class="marg025 btn-group" id="select_clientes2" >
+                    <div class="marg025 btn-group" id="select_clientes" >
                         <select name="proxDias" class="form-control" size="1" v-model="valor_clientes" id="select_clientes">
                           <option v-for="option in datos_select.clientes" v-bind:value="option">
                             @{{ option }}
@@ -221,8 +221,6 @@ const app = new Vue({
             nCells[1].innerHTML = accounting.formatMoney(totalUsd, "$", 2);
         }
       });
-      $('#select_cotizaciones').select2();
-      $('#select_proyectos').select2();
       $("#fechas_container").append($("#fecha_ini_control"));
       $("#fechas_container").append($("#fecha_fin_control"));
       $("#fechas_container").append($("#select_cotizaciones"));
