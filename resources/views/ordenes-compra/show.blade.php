31eba87380f8fde690dd61dd39009e9cc47de3d2
Eduardo <Eduardo.santana@tigears.com>
2021-11-12 07:07:28 -0600

    ajuste

--- resources/views/ordenes-compra/show.blade.php
+++ resources/views/ordenes-compra/show.blade.php
@@ -114,6 +114,7 @@
                     <thead>
                       <tr>
                         <th>Producto</th>
+                        <th>Área</th>
                         <th>Comentarios</th>
                         <th>Cantidad</th>
                         <th>Conversión</th>
@@ -126,6 +127,7 @@
                       @foreach($orden->entradas as $entrada)
                       <tr>
                         <td>{{$entrada->producto->nombre}}</td>
+                        <td>{{$entrada->area}}</td>
                         <td>{{$entrada->comentarios}}</td>
                         <td>{{$entrada->cantidad}} {{$entrada->medida}}</td>
                         <td>{{$entrada->conversion}}</td>
