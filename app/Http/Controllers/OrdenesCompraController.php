700cfc14f4a60441b77a53b0e0863cab48a3c006
Eduardo <Eduardo.santana@tigears.com>
2021-11-14 21:48:39 -0600

    ajuste fotos

--- app/Http/Controllers/OrdenesCompraController.php
+++ app/Http/Controllers/OrdenesCompraController.php
@@ -440,7 +440,6 @@ class OrdenesCompraController extends Controller
      */
     public function update(Request $request, ProyectoAprobado $proyecto, OrdenCompra $orden)
     {
-        dd($request);
         
         $validator = Validator::make($request->all(), [
             'proyecto_id' => 'required',
