e888d7cc2ff6e679d3eb657c8a53ce7ebb807741
Eduardo <Eduardo.santana@tigears.com>
2021-11-14 22:38:21 -0600

    ajustes

--- resources/views/ordenes-compra/edit.blade.php
+++ resources/views/ordenes-compra/edit.blade.php
@@ -873,12 +873,7 @@ const app = new Vue({
         delete entrada.producto;
       });
 
-      const formData = new FormData();
-
-      buildFormData(formData, orden);
-
-      console.log(formData);
-      console.log(orden);
+      var formData = objectToFormData(orden, {indices: true});
 
       this.cargando = true;
       axios.put('/proyectos-aprobados/{{$proyecto->id}}/ordenes-compra/{{$orden->id}}', formData,{headers: {'Content-Type': 'multipart/form-data'}
