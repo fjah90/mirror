57ca511a7548e3d1eaebda87c2e33a78e5cbfd5a
Eduardo <Eduardo.santana@tigears.com>
2021-11-24 16:06:02 -0600

    ajuste fotos

--- resources/views/ordenes-compra/edit.blade.php
+++ resources/views/ordenes-compra/edit.blade.php
@@ -504,6 +504,15 @@ Vue.config.devtools = true;
   }
 }
 
+// Used for creating a new FileList in a round-about way
+    function FileListItem(a) {
+        a = [].slice.call(Array.isArray(a) ? a : arguments)
+        for (var c, b = c = a.length, d = !0; b-- && d;) d = a[b] instanceof File
+        if (!d) throw new TypeError("expected argument to FileList is File or array of File objects")
+        for (b = (new ClipboardEvent("")).clipboardData || new DataTransfer; c--;) b.items.add(a[c])
+        return b.files
+    }
+
 
 const app = new Vue({
   el: '#content',
