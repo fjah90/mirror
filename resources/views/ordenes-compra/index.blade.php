41ee0f17c9d39f558234398f68200de3bbe07e13
Eduardo <Eduardo.santana@tigears.com>
2021-11-24 16:43:05 -0600

    ajuste fotos

--- resources/views/ordenes-compra/index.blade.php
+++ resources/views/ordenes-compra/index.blade.php
@@ -328,8 +328,10 @@ const app = new Vue({
         });
       },//cancelar
       sumartotal(){
+        console.log(this.ordenModal.monto_total_pagar);
         if (this.ordenModal.monto_total_pagar == NaN) {
           this.ordenModal.monto_total_pagar = 0;
+          console.log(this.ordenModal.monto_total_pagar);
         }
         if (this.ordenModal.monto_total_producto != null || this.ordenModal.monto_total_producto != "" ) {
             this.ordenModal.monto_total_pagar += this.ordenModal.monto_total_producto;  
