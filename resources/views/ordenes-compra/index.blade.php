b56eb3465a9a4e17060c0e30b8858cb402fb7cca
Eduardo <Eduardo.santana@tigears.com>
2021-12-02 19:34:44 -0600

    ajuste

--- resources/views/ordenes-compra/index.blade.php
+++ resources/views/ordenes-compra/index.blade.php
@@ -92,7 +92,7 @@
                     </button>
                     
                     <button v-if="orden.status=='Aprobada'" class="btn btn-xs btn-purple"
-                      title="Confirmar" @click="ordenModal=orden; openConfirmar=true; ordenModal.monto_flete=orden.flete;ordenModal.monto_total_producto=orden.total;">
+                      title="Confirmar" @click="ordenModal=orden; openConfirmar=true; ordenModal.monto_flete=orden.flete;ordenModal.monto_total_producto=orden.total;ordenModal.tax=orden.iva;">
                       <i class="fas fa-clipboard-check"></i>
                     </button>
                     @endrole
