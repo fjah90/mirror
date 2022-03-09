24489c67d8ac1034b83a4e1d139a852b9443addb
Eduardo <Eduardo.santana@tigears.com>
2022-03-08 20:33:07 -0600

    ajuste

--- resources/views/reportes/compras.blade.php
+++ resources/views/reportes/compras.blade.php
@@ -318,9 +318,9 @@ const app = new Vue({
           datosfinal.datos.push(data);
         }
 
-        console.log(datosfinal);
+        //console.log(datosfinal);
 
-        //var formData = objectToFormData(datos, {indices: true});
+        var formData = objectToFormData(datosfinal, {indices: true});
 
         //console.log(datos);
 
