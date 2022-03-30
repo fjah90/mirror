df89141474bdb2e8b35fc7abf88cfa395381eee5
Eduardo <Eduardo.santana@tigears.com>
2022-03-30 17:10:46 -0600

    ajuste

--- app/Http/Controllers/ProspectosController.php
+++ app/Http/Controllers/ProspectosController.php
@@ -51,7 +51,7 @@ class ProspectosController extends Controller
             */
         $prospectos = Prospecto::with('cliente', 'ultima_actividad','ultima_actividad.tipo', 'proxima_actividad.tipo', 'user','cotizaciones')
         ->where('user_id', $user->id)
-        ->whereBetween('ultima_actividad.created_at', [$inicio, $anio]);
+        ->whereBetween('ultima_actividad.created_at', [$inicio, $anio])
         ->has('cliente')
         ->get();
 
