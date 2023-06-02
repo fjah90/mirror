8e84bf3b49db108bd8b4bf5dd5efdee287c36e50
Eduardo <eduardo.santana@tigears.com>
2023-06-01 21:40:28 -0500

    tareas de prospectos

--- resources/views/prospectos/indexprospectos.blade.php
+++ resources/views/prospectos/indexprospectos.blade.php
@@ -37,7 +37,7 @@
 
           <h3 class="panel-title">
             <div class="p-10" style="display:inline-block">
-              Vendedor   
+              Diseñador  
               @role('Administrador|Dirección')
                 <select class="form-control" @change="cargar()" v-model="usuarioCargado" style="width:auto;display:inline-block;">
                   <option value="Todos">Todos</option>
