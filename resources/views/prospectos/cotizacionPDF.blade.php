7e1794a91fb4748797bf4ff563cc8b0b8ef6dcad
Fernando Aponte <fernando.aponte@789.mx>
2023-09-05 18:33:07 -0400

    fix: remove fontfamily

--- resources/views/prospectos/cotizacionPDF.blade.php
+++ resources/views/prospectos/cotizacionPDF.blade.php
@@ -2,18 +2,18 @@
 <html lang="en">
 
 <head>
-    <link rel="stylesheet" href="{{ asset('css/latofonts.css') }}" rel="stylesheet">
+    {{-- <link rel="stylesheet" href="{{ asset('css/latofonts.css') }}" rel="stylesheet"> --}}
     <meta charset="UTF-8">
     <style>
         html {
-            font-family: "LatoWeb";
+            /* font-family: "LatoWeb"; */
             -webkit-text-size-adjust: 100%;
             -ms-text-size-adjust: 100%;
         }
 
         body {
             margin: 25px;
-            font-family: "LatoWeb";
+            /* font-family: "LatoWeb"; */
             font-size: 10px;
             line-height: 1.1;
             color: #333;
@@ -32,7 +32,7 @@
         .h4,
         .h5,
         .h6 {
-            font-family: "LatoWeb";
+            /* font-family: "LatoWeb"; */
             font-weight: 500;
             color: inherit;
         }
