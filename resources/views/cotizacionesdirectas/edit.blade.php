db57f1c7e68e9891624a37e6ad49f350e40f5c18
Eduardo <eduardo.santana@tigears.com>
2023-09-29 19:05:27 -0500

    ajuste

--- resources/views/cotizacionesdirectas/edit.blade.php
+++ resources/views/cotizacionesdirectas/edit.blade.php
@@ -2203,7 +2203,7 @@
                     });
 
                     this.cargando = true;
-                    axios.post('/productos/' + this.descripcion.id + '/updateVisibilidad', formData, {
+                    axios.post('/productos/' + descripcion.id + '/updateVisibilidad', formData, {
                             headers: {
                                 'Content-Type': 'multipart/form-data'
                             }
